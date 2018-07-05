79b002ffe242e0b3c22158f4e0782c4626e57dd2
Jianfeng-NSL <jianfenw@usc.edu>
2018-07-04 22:40:42 -0700

    fix a layer bug

--- tree/linux_tree.py
+++ tree/linux_tree.py
@@ -20,8 +20,11 @@ Inside the str. Replace the substring a with string b
 
 """
 
+
 class tree_cmd(object):
 	def __init__(self):
+		self.dir_count = 0
+		self.file_count = 0
 		pass
 
 	def get_tree(self, target_dir, option):
@@ -46,7 +49,10 @@ class tree_cmd(object):
 				# layer starts from 1
 				# is_last_layer[layer]
 				names = relative_filepath.split('/')
-				curr_layer = len(names) - 1
+				curr_layer = len(names)
+				#if "screen-shot.png" in relative_filepath:
+				print relative_filepath, curr_layer
+
 				if len(is_last_dir) == curr_layer: # make len(is_last_dir) == curr_layer+1
 					is_last_dir.append(False)
 				else:
@@ -70,6 +76,7 @@ class tree_cmd(object):
 		# print the formatted output
 		for i in range(len(tree_list)):
 			print tree_list[i]
+		print "\n%d directories, %d files" %(self.dir_count, self.file_count)
 		return
 
 	def get_list(self, target_dir, layer, op):
@@ -88,13 +95,16 @@ class tree_cmd(object):
 			complete_file = os.path.join(target_dir, file)
 			if os.path.isdir(complete_file):
 				# this is a dir
+				self.dir_count += 1
 				target_file_list.append(complete_file + ' *')
 				target_file_list += self.get_list(complete_file, layer+1, op)
 				last_file = complete_file + ' *'
 			elif op == '-d':
+				self.file_count += 1
 				pass
 			else:
 				# this is a file
+				self.file_count += 1
 				target_file_list.append(complete_file)
 				last_file = complete_file
 
